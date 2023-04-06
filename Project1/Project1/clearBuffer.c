20262e64f229c9a5212ef3fc1dee3cb88ec81848
OwenOliveira <101082127+OwenOliveira@users.noreply.github.com>
2023-04-06 11:50:04 -0400

    Added a few extra comments and reviewed the rest of the code

--- Project1/Project1/clearBuffer.c
+++ Project1/Project1/clearBuffer.c
@@ -1,3 +1,9 @@
+// Icebox Sails
+// Funciton file for clearBuffer function
+// CSCN71030 - 23W - Sec1 - Team Based Software Development
+// Group 13 - Winter 2023
+// Version 1.0
+
 #include "clearBuffer.h"
 #include <stdio.h>
 
--- Project1/Project1/userInput.c
+++ Project1/Project1/userInput.c
@@ -1,6 +1,12 @@
+// Icebox Sails
+// User Input funciton file
+// CSCN71030 - 23W - Sec1 - Team Based Software Development
+// Group 13 - Winter 2023
+// Version 1.0
 #include "userInput.h"
 #include <stdio.h>
 
+// This function takes no arguments and returns a pointer to a character array
 char* userInput(void)
 {
 	char input = getc(stdin);
